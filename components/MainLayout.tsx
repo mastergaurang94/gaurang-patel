80f6b74f546f33243d28208d01749486e9e9828a
Gaurang Patel <mastergaurang94@gmail.com>
2022-12-12 12:31:26 -0600

    try again

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={`container mx-auto py-8 px-4`} style={{ fontFamily: "Vollkorn, serif", width: '960px'}}>
-      <header className="container">
+    <div className={`container mx-auto max-w-2/3 sm:max-w-full py-8 px-4`} style={{ fontFamily: "Vollkorn, serif" }}>
+      <header className="container mx-auto">
         <div className={`flex flex-col justify-between`}>
           <div className={`flex justify-start`}>
             <Link href="/">
@@ -21,7 +21,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
       <main className="container mx-auto">{children}</main>
 
-      <footer className="py-2">
+      <footer className="container mx-auto py-2">
         <div className="container">
           &copy; 2022 Gaurang Patel
         </div>
