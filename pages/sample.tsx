73abb48daa7ed32ca971a7e10df3a6f896d6db97
Gaurang Patel <mastergaurang94@gmail.com>
2023-08-01 13:07:14 -0500

    Comment out sample page for now

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import MySvg from "../8f38871f9067eb7c5904a337c0ddb28f99499cbbf2977d15fb1c7f3160097d2c/0.svg";
+// import React from "react";
+// import MySvg from "../8f38871f9067eb7c5904a337c0ddb28f99499cbbf2977d15fb1c7f3160097d2c/0.svg";
 
-//example page for displaying svgs
-const MyComponent: React.FC = () => {
-  return <MySvg />;
-};
+// //example page for displaying svgs
+// const MyComponent: React.FC = () => {
+//   return <MySvg />;
+// };
 
-export default MyComponent;
+// export default MyComponent;
